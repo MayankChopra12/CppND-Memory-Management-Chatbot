12e6319bba6103cbfad701fe41e34d8da56fc7e2
MayankChopra12 <52525311+MayankChopra12@users.noreply.github.com>
2020-06-02 17:52:55 +0530

    Update graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -1,8 +1,12 @@
 #include "graphedge.h"
 #include "graphnode.h"
 
+// For debugging
+#include <iostream>
+
 GraphNode::GraphNode(int id)
 {
+    std::cout << "In GraphNode Constructor" << std::endl;
     _id = id;
 }
 
@@ -11,7 +15,9 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    //delete _chatBot; 
+    std::cout << "In GraphNode Destructor" << std::endl;
+
+    // delete _chatBot; 
 
     ////
     //// EOF STUDENT CODE
@@ -27,23 +33,31 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+// void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> &&edge)
 {
     _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot chatBot)
+void GraphNode::MoveChatbotHere(ChatBot &&chatbot)
 {
-    _chatBot = std::move(chatBot);
+    _chatBot = ChatBot(std::move(chatbot));
+    _chatBot.GetChatLogicHandle()->SetChatbotHandle(&_chatBot);
+    // _chatBot.SetImageHandle(_chatBot.GetImageHandle()); // Set the image handle to the previous image handle
     _chatBot.SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(std::move(_chatBot));
-    //_chatBot = nullptr; // invalidate pointer at source
+    ChatBot tempbot = ChatBot();
+    tempbot.SetRootNode(_chatBot.GetRootNode()); // Set the root node
+    tempbot.SetChatLogicHandle(_chatBot.GetChatLogicHandle());
+    tempbot.SetImageHandle(_chatBot.GetImageHandle());
+    newNode->MoveChatbotHere(std::move(tempbot));    
+
+    _chatBot.SetImageHandle(nullptr); // invalidate image pointer so only one node deletes the image in the end
 }
 ////
 //// EOF STUDENT CODE
@@ -53,6 +67,7 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
+    // return _childEdges[index];
     return _childEdges[index].get();
 
     ////
